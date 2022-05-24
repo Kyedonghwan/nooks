ddc74612eddca4ad3c696d153111a1ae0fdee7eb
Kyedonghwan <kdh643737@gmail.com>
2022-05-24 19:49:19 +0900

    20220524

---
+++ useInput/useBeforeLeave.js
@@ -0,0 +1,18 @@
+import { useEffect } from 'react';
+
+const useBeforeLeave = (onBefore) => {
+    if (!onBefore || typeof onBefore !== "function") {
+      return;
+    }
+    const handle = (e) => {
+      const { clientY } = e;
+      if (clientY <= 0) {
+        onBefore();
+      }
+    };
+  
+    useEffect(() => {
+      document.addEventListener("mouseleave", handle);
+      return () => document.removeEventListener("mouseleave", handle);
+    }, []);
+  };
